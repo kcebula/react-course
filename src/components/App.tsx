15cb09ee2975673572f8a94c3d906ae9486bc2d9
Karol Cebula <kcebula@digitalaviationservices.com>
2019-05-15 06:47:40 +0200

    Videos start

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,37 +1,7 @@
-import React, { SyntheticEvent } from 'react';
-import { SearchBar } from './SearchBar';
-import { unsplash } from '../api/unsplash';
-import { ImageList } from './ImageList';
-
+import React from 'react';
 
 export class App extends React.Component {
-
-  state = {
-    query: '',
-    images: []
-  };
-
-  async onSearchSubmit(event: SyntheticEvent, query: string) {
-    event.preventDefault();
-
-    if (this.state.query === query) {
-      return;
-    }
-
-    unsplash.get('/search/photos',{
-      params: {
-        query: query
-      }
-    }).then(response => {
-      this.setState({ images: response.data.results });
-    });
-  };
-
   render() {
-    return <div className="ui container">
-      <SearchBar onSearchSubmit={(event: SyntheticEvent, query: string) => this.onSearchSubmit(event, query)}/>
-      <p>Got {this.state.images.length} images</p>
-      <ImageList images={this.state.images} />
-    </div>
+    return <div>Videos</div>;
   }
 }
